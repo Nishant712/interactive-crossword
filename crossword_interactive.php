1d43bd0cb584539d8feb945a2de98f9d7b4e45b4
Nishant Barma <savinay.90@gmail.com>
2018-04-24 00:02:25 +0000

    ok

--- crossword_interactive.php
+++ crossword_interactive.php
@@ -11,9 +11,10 @@
 	<title>Qurossword - A javascript crossword puzzle plugin</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script src="js/jquery.crossword.js"></script>
-	<script src="js/script.js"></script>
+	<script src="js/interactive_crossword.js"></script>
+	<script src="js/script2.js"></script>
 	<script src="js/change_view.js"></script>
+	<script src="js/check.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -131,11 +132,7 @@
 </head>
 
 <body>
-<div id="change-view">
-	<a href="#" class="btn-3d blue" onclick="checkCrossword()">Check</a>
-</div>	
 <div id="puzzle-wrapper"><!-- crossword puzzle appended here --></div>
-<div id="puzzle-wrapper-interactive"><!-- crossword puzzle appended here --></div>
 <span id="audio-error"></span>
 <span id="audio-success"></span>
 
---
+++ js/check.js
@@ -0,0 +1,5 @@
+var cells = sessionStorage.getItem("cells");
+console.log(cells);
+// for(var i = 0; i < cells.length; i++) {
+//     console.log(cells[i].value);
+// }
